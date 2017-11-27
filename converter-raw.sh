2367615f2950c90d970855e43e627d4734c9cbae
zppro <zpprozppro@gmail.com>
2017-11-27 10:02:39 +0800

    转换默认脚本

--- converter-raw.sh
+++ converter-raw.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# File: converter.sh
+# File: converter-raw.sh
 # Date: August 19th, 2016
 # Time: 18:56:52 +0800
 # Author: kn007 <kn007@126.com>
 # Blog: https://kn007.net
 # Link: https://github.com/kn007/silk-v3-decoder
-# Usage: sh converter.sh silk_v3_file/input_folder output_format/output_folder flag(format)
+# Usage: sh converter-raw.sh silk_v3_file/input_folder output_format/output_folder flag(format)
 # Flag: not define   ----  not define, convert a file
 #       other value  ----  format, convert a folder, batch conversion support
 # Requirement: gcc ffmpeg
@@ -67,7 +67,7 @@ if [ ! -f "$1.pcm" ]; then
 	[ -f "${1%.*}.$2" ]&&echo -e "${GREEN}[OK]${RESET} Convert $1 to ${1%.*}.$2 success, ${YELLOW}but not a silk v3 encoded file.${RESET}"&&exit
 	echo -e "${YELLOW}[Warning]${RESET} Convert $1 false, maybe not a silk v3 encoded file."&&exit
 fi
-ffmpeg -y -f s16le -ar 12000 -ac 2 -i "$1.pcm" -f wav -ar 16000 -ac 1 "${1%.*}.$2" > /dev/null 2>&1
+ffmpeg -y -f s16le -ar 24000 -ac 1 -i "$1.pcm" "${1%.*}.$2" > /dev/null 2>&1
 ffmpeg_pid=$!
 while kill -0 "$ffmpeg_pid"; do sleep 1; done > /dev/null 2>&1
 rm "$1.pcm"
--- converter.sh
+++ converter.sh
@@ -67,7 +67,7 @@ if [ ! -f "$1.pcm" ]; then
 	[ -f "${1%.*}.$2" ]&&echo -e "${GREEN}[OK]${RESET} Convert $1 to ${1%.*}.$2 success, ${YELLOW}but not a silk v3 encoded file.${RESET}"&&exit
 	echo -e "${YELLOW}[Warning]${RESET} Convert $1 false, maybe not a silk v3 encoded file."&&exit
 fi
-ffmpeg -y -f s16le -ar 24000 -ac 1 -i "$1.pcm" "${1%.*}.$2" > /dev/null 2>&1
+ffmpeg -y -f s16le -ar 12000 -ac 2 -i "$1.pcm" -f wav -ar 16000 -ac 1 "${1%.*}.$2" > /dev/null 2>&1
 ffmpeg_pid=$!
 while kill -0 "$ffmpeg_pid"; do sleep 1; done > /dev/null 2>&1
 rm "$1.pcm"
